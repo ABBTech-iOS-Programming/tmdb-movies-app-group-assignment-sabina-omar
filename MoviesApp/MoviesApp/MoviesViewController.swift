df0b5ff7713472cf7a1acfad43b55ca07520134d
Sabina <sebinekerimli067@gmail.com>
2025-12-29 23:14:51 -0800

    rename ViewController to MovieViewController

---
+++ MoviesApp/MoviesApp/MoviesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by SabinaKarimli on 29.12.25.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
