c430922fbb85e9fd813eeb0727a2b029cc073120
Sabina <sebinekerimli067@gmail.com>
2026-01-09 03:00:36 -0800

    fix tabbar

--- MoviesApp/MoviesApp/Screens/MainTabBar/MainTabBarController.swift
+++ MoviesApp/MoviesApp/Screens/MainTabBar/MainTabBarController.swift
@@ -18,7 +18,7 @@ class MainTabBarController: UITabBarController {
     private func setupBar() {
         let homeVC = MoviesBuilder.build()
         let searchVC = SearchViewController()
-        let watchListVC = WatchlistViewController()
+        let watchListVC = WatchListViewController()
         let homeNavBar = UINavigationController(rootViewController: homeVC)
         let searchNavBar = UINavigationController(rootViewController: searchVC)
         let watchListNavBar = UINavigationController(rootViewController: watchListVC)
